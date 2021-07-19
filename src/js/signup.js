a09ceb2ce06f159aec45e9bee054f6e2c49b01d6
Marina Tatic <tatic.marina8@gmail.com>
2021-07-19 12:30:06 +0200

    Scroll animations added and signup scroll to the top of the form on button click added.

--- src/js/signup.js
+++ src/js/signup.js
@@ -1,19 +1,8 @@
 var signup = {
 
-	init: function() {
-
-		// var inputTexts = document.querySelectorAll('input[type="text"]');
-		// var isTouchDevice = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
+	init: function () {
 		var btnNext = document.querySelectorAll('.signup-button-next');
 		var btnPrev = document.querySelectorAll('.signup-button-back');
-
-		// if (isTouchDevice && inputTexts.length) {
-		// 	for (var i = 0; i < inputTexts.length; i++) {
-		// 		if (!inputTexts[i].classList.contains('js-range')) {
-		// 			inputTexts[i].type = 'number';
-		// 		}
-		// 	}
-		// }
 
 		if (btnNext) {
 			for (var i = 0; i < btnNext.length; i++) {
@@ -30,49 +19,37 @@ var signup = {
 
 	goToNextPage: function () {
 		var pages = document.querySelectorAll('.page');
-		var btnNext = document.querySelectorAll('.signup-button-next');
-		var btnIndex = [...btnNext].indexOf(this);
 
 		var pageParent = this.closest('.page');
+		var bodyRect = document.body.getBoundingClientRect(),
+			pageRect = pageParent.getBoundingClientRect(),
+			pageOffset = pageRect.top - bodyRect.top;
 		var pageParentIndex = [...pages].indexOf(pageParent);
 		pages[pageParentIndex].classList.remove('page-active');
 		pages[pageParentIndex + 1].classList.add('page-active');
 
-		//not smooth scroll to top
-		// window.scrollTo(0, 0);
-		// window.scrollTo({ top: 0, behavior: 'smooth' });
-
-		var pageContainer = document.querySelectorAll('.signup-container')[0];
-		var pageActiveHeight = document.querySelectorAll('.page-active')[0].offsetHeight;
-		pageContainer.style.minHeight = pageActiveHeight + 'px';
+		//smooth scroll to top
+		window.scrollTo({ top: pageOffset, behavior: 'smooth' });
 	},
 
-	goToPrevPage: function() {
+	goToPrevPage: function () {
 		var pages = document.querySelectorAll('.page');
 		var btnPrev = document.querySelectorAll('.signup-button-back');
-		// var btnIndex = [...btnPrev].indexOf(this);
 		var pageContainer = document.querySelectorAll('.signup-container')[0];
 
 		var pageParent = this.closest('.page');
+		var bodyRect = document.body.getBoundingClientRect(),
+			pageRect = pageParent.getBoundingClientRect(),
+			pageOffset = pageRect.top - bodyRect.top;
 		var pageParentIndex = [...pages].indexOf(pageParent);
 		pages[pageParentIndex].classList.remove('page-active');
-		// if (btnIndex !== 0) {
 		pages[pageParentIndex - 1].classList.add('page-active');
 
-		var pageActiveHeight = document.querySelectorAll('.page-active')[0].offsetHeight;
-		pageContainer.style.minHeight = pageActiveHeight + 'px';
-		// }
-		// if (btnIndex === 0) {
-		// 	templatesignup.classList.remove('signup-page-active');
-		// 	pageContainer.style.minHeight = '';
-		// }
-
-		//not smooth scroll to top
-		// window.scrollTo(0, 0);
-		// window.scrollTo({ top: 0, behavior: 'smooth' });
+		//smooth scroll to top
+		window.scrollTo({ top: pageOffset, behavior: 'smooth' });
 	}
 };
 
-uk.ready(function() {
+uk.ready(function () {
 	signup.init();
 });
