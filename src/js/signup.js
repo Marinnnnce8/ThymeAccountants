9c871a2998b68fa4017e84088654b1bb1c21e670
Marina Tatic <tatic.marina8@gmail.com>
2021-07-23 14:15:06 +0200

    Signup thank you message added and mobile swipe icon position tweaked.

--- src/js/signup.js
+++ src/js/signup.js
@@ -3,6 +3,7 @@ var signup = {
 	init: function () {
 		var btnNext = document.querySelectorAll('.signup-button-next');
 		var btnPrev = document.querySelectorAll('.signup-button-back');
+		var btnLater = document.querySelectorAll('.signup-button-later')[0];
 
 		if (btnNext) {
 			for (var i = 0; i < btnNext.length; i++) {
@@ -15,6 +16,10 @@ var signup = {
 				btnPrev[i].addEventListener('click', this.goToPrevPage, false);
 			}
 		}
+		
+		if (btnLater) {
+			btnLater.addEventListener('click', this.goToThankYouPage, false);
+		}
 	},
 
 	goToNextPage: function () {
@@ -26,7 +31,11 @@ var signup = {
 			pageOffset = pageRect.top - bodyRect.top;
 		var pageParentIndex = [...pages].indexOf(pageParent);
 		pages[pageParentIndex].classList.remove('page-active');
-		pages[pageParentIndex + 1].classList.add('page-active');
+		if (pages[pageParentIndex + 1].classList.contains('page-thank-you')) {
+			pages[pageParentIndex + 2].classList.add('page-active');
+		} else {
+			pages[pageParentIndex + 1].classList.add('page-active');
+		}
 
 		//smooth scroll to top
 		window.scrollTo({ top: pageOffset, behavior: 'smooth' });
@@ -34,8 +43,6 @@ var signup = {
 
 	goToPrevPage: function () {
 		var pages = document.querySelectorAll('.page');
-		var btnPrev = document.querySelectorAll('.signup-button-back');
-		var pageContainer = document.querySelectorAll('.signup-container')[0];
 
 		var pageParent = this.closest('.page');
 		var bodyRect = document.body.getBoundingClientRect(),
@@ -44,6 +51,22 @@ var signup = {
 		var pageParentIndex = [...pages].indexOf(pageParent);
 		pages[pageParentIndex].classList.remove('page-active');
 		pages[pageParentIndex - 1].classList.add('page-active');
+
+		//smooth scroll to top
+		window.scrollTo({ top: pageOffset, behavior: 'smooth' });
+	},
+
+	goToThankYouPage: function () {
+		var pageThankYou = document.querySelectorAll('.page-thank-you')[0];
+		var pages = document.querySelectorAll('.page');
+
+		var pageParent = this.closest('.page');
+		var bodyRect = document.body.getBoundingClientRect(),
+			pageRect = pageParent.getBoundingClientRect(),
+			pageOffset = pageRect.top - bodyRect.top;
+		var pageParentIndex = [...pages].indexOf(pageParent);
+		pages[pageParentIndex].classList.remove('page-active');
+		pageThankYou.classList.add('page-active');
 
 		//smooth scroll to top
 		window.scrollTo({ top: pageOffset, behavior: 'smooth' });
